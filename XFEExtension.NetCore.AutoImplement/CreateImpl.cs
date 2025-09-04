499508042a411299ee81288c9b9b516bb7da42b7
XFEstudio <mail@xfegzs.com>
2025-09-04 13:22:26 +0800

    将源生成改为增量生成器

--- XFEExtension.NetCore.AutoImplement/CreateImpl.cs
+++ XFEExtension.NetCore.AutoImplement/CreateImpl.cs
@@ -4,4 +4,19 @@
 /// 创建一个类的实现类
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
-public class CreateImpl : Attribute { }
+public class CreateImpl : Attribute
+{
+    public string? ClassName { get; set; }
+    public string? NameSpace { get; set; }
+    public string? Modifier { get; set; }
+    public CreateImpl() { }
+    public CreateImpl(string className)
+    {
+        ClassName = className;
+    }
+    public CreateImpl(string className, string modifier)
+    {
+        ClassName = className;
+        Modifier = modifier;
+    }
+}
